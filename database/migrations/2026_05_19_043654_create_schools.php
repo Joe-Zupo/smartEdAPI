c22cb32a752b823f77fc187f14d2908451aa6c58
Joe-Zupo <josephmallari.industry.ict@gmail.com>
2026-05-19 12:41:16 +0800

    School and School Types migration, model and controller

---
+++ database/migrations/2026_05_19_043654_create_schools.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            //School Information
+            $table->id();
+            $table->string('school_name');
+            $table->string('school_code');
+            $table->year('year_established');
+            $table->foreignID('school_id_type')->constrained('school_types')->cascadeOnDelete();
+            
+            //location
+            $table->string('address');
+            $table->decimal('longitude', 8, 2);
+            $table->decimal('latitude', 8, 2);
+
+            //other
+            $table->string('image')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schools');
+    }
+};
