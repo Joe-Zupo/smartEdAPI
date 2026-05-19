cb19e86c14ceaeb48f3fafcfa4a27fa64f9bc185
Joe-Zupo <josephmallari.industry.ict@gmail.com>
2026-05-19 13:55:39 +0800

    Helper Methods and Controller Traits

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\autoPaginator;
+use App\responseAPI;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 abstract class Controller
 {
-    //
+    use autoPaginator, responseAPI, AuthorizesRequests;
 }
---
+++ app/autoPaginator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait autoPaginator
+{
+    protected function paginateReturn(LengthAwarePaginator $lap){
+        return [
+            'current_page' => $lap->currentPage(),
+            'last_page' => $lap->lastPage(),
+            'per_page' => $lap->perPage(),
+            'total' => $lap->total(),
+            'prev_page_url' => $lap->previousPageUrl(),
+            'next_page_url' => $lap->nextPageUrl(),
+        ];
+    }
+}
