fd089d60c9c05b7de7dd874fdc88191b42733ad8
Aeron <74293138+iamaeron@users.noreply.github.com>
2026-05-21 12:31:24 +0800

    reverted back to token-based auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -94,30 +94,29 @@ class AuthController extends Controller
      */
     public function logout(Request $request)
     {
+        // Auth::guard('web')->logout();
 
-        Auth::guard('web')->logout();
+        // activity('Logged Out')
+        //     ->causedBy($request->user())
+        //     ->performedOn($request->user())
+        //     ->withProperties([
+        //         'datetime' => now()->format('Y-m-d h:i:s A'),
+        //     ])
+        //     ->log('User logged out');
+        // return $this->success('User Logged Out Successfully');
 
-        activity('Logged Out')
-            ->causedBy($request->user())
-            ->performedOn($request->user())
-            ->withProperties([
-                'datetime' => now()->format('Y-m-d h:i:s A'),
-            ])
-            ->log('User logged out');
-        return $this->success('User Logged Out Successfully');
-
-        // if ($request->user()) {
-        //     $token = $request->user()->currentAccessToken();
-        //     return $token;
-        //     // if ($token) {
-        //     //     $token->delete();
-        //     //     activity()
-        //     //         ->causedBy($request->user())
-        //     //         ->performedOn($request->user())
-        //     //         ->log('User logged out');
-        //     //     return $this->success('User Logged Out Successfully');
-        //     // }
-        //     // return $this->error('Invalid token used');
-        // }
+        if ($request->user()) {
+            $token = $request->user()->currentAccessToken();
+            return $token;
+            // if ($token) {
+            //     $token->delete();
+            //     activity()
+            //         ->causedBy($request->user())
+            //         ->performedOn($request->user())
+            //         ->log('User logged out');
+            //     return $this->success('User Logged Out Successfully');
+            // }
+            // return $this->error('Invalid token used');
+        }
     }
 }
