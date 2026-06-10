dfd4770526be1529a845b2531cbd25fbf3b22feb
Joe-Zupo <josephmallari.industry.ict@gmail.com>
2026-06-10 14:05:47 +0800

    (MINOR) Fix to Returned values for Resource-Data

--- app/Http/Controllers/ResourceDataController.php
+++ app/Http/Controllers/ResourceDataController.php
@@ -110,7 +110,7 @@ class ResourceDataController extends Controller
                         'id' => $academicYear->id,
                         'name' => $academicYear->academic_year,
                     ],
-                    //'items' => ResourceDataResource::collection($resources),
+                    'items' => ResourceDataResource::collection($resources),
                     'totals_by_resource' => $totals,
                 ],
                 'pagination' => $getAll ? null : $this->paginateReturn($resources)
